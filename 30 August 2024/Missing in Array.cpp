c31af46c8ff08b7001d2485d2daca23404524379
Vaibhavi <141433438+alfredoLoverdotcpp@users.noreply.github.com>
2024-08-30 01:32:44 +0530

    Create Missing in Array.cpp

---
+++ 30 August 2024/Missing in Array.cpp
@@ -0,0 +1,19 @@
+/* Input: n = 5, arr[] = [1,2,3,5]
+Output: 4
+*/
+//TC - O(N),SC - O(1)
+class Solution {
+  public:
+
+    // Note that the size of the array is n-1
+    int missingNumber(int n, vector<int>& arr) {
+
+        int sum=(n*(n+1))/2;
+        int s=0;
+        for(int i=0; i<n-1; ++i)          
+        {
+            s+=arr[i];
+        }
+        return(sum-s);
+    }
+};
